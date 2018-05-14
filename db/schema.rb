44b49bfa759c308877ba30a47aeaf7ac6abd84ac
Javier Machin <javier_machin_acosta@hotmail.com>
2018-05-14 12:08:57 +0100

    Add event model and relations

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180513101543) do
+ActiveRecord::Schema.define(version: 20180514105606) do
+
+  create_table "events", force: :cascade do |t|
+    t.string "name"
+    t.text "description"
+    t.date "date"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "creator_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
