afc9fe4b1e4f33802094a90d5a5210fe9df0ce8a
Javier Machin <javier_machin_acosta@hotmail.com>
2018-06-10 12:44:40 +0100

    Add letter opener as delivery method for development mailer

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -25,7 +25,9 @@ Rails.application.configure do
 
     config.cache_store = :null_store
   end
-
+  
+  config.action_mailer.delivery_method = :letter_opener
+  config.action_mailer.perform_deliveries = true
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
 
@@ -51,4 +53,6 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+
+
 end
