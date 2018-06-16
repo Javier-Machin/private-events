00e575854af96fde1f5464170cc596b57069db5d
Javier Machin <javier_machin_acosta@hotmail.com>
2018-06-16 16:53:11 +0100

    Change mailer host

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -15,7 +15,7 @@ Rails.application.configure do
   config.action_controller.perform_caching = true
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  host = 'stormy-mountain-23973.herokuapp.com'
+  host = 'social-boost-app.herokuapp.com'
   config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
     :address        => 'smtp.sendgrid.net',
